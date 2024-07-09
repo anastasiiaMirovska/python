a71b2726ba6ff65a544788137f8343e67ad449ae
Anastasiia Mirovska <anastasiia.mirovska@gmail.com>
2024-07-09 21:45:40 +0300

    Some changes in strings_task_2.py

--- homework1/strings_task_2.py
+++ homework1/strings_task_2.py
@@ -2,7 +2,7 @@ my_input = input("Введіть текст з числами:  ")
 output = ""
 
 for char in my_input:
-    if char.isalpha() or char.isspace():
+    if not char.isnumeric():
         if len(output) == 0 or output[-1] == ",":
             continue
         else:
