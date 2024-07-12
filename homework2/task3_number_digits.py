8f1fa62c677fd2d95046b79bebaa930e87b93294
Anastasiia Mirovska <anastasiia.mirovska@gmail.com>
2024-07-12 17:48:15 +0300

    Slight changes

--- homework2/task3_number_digits.py
+++ homework2/task3_number_digits.py
@@ -1,4 +1,5 @@
 def expanded_form(number: str | int):
+    number = int(number)  # Це на випадок, якщо випаде булеве значення
     number = str(number)
     numbers: list[str] = list()
     length = len(number)
@@ -6,12 +7,10 @@ def expanded_form(number: str | int):
         numbers.append(number[i]+"0"*(length-i-1))
     print(" + ".join(numbers))
 
-expanded_form(23)
-
+expanded_form(True)
 
 
 # Покращена версія:
-
 def expanded_form_better(number: str | int):
     if not isinstance(number, (int, str)):
         print("Неправильний тип даних")
